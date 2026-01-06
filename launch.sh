469ea693522620543d7c2a543ff1db332e1e0f50
Lodomo <lodomo.dev@gmail.com>
2026-01-06 11:30:56 -0800

    Updating launch script

--- launch.sh
+++ launch.sh
@@ -3,4 +3,9 @@
 GUNICORN_CONF=./src/gunicorn.conf.py
 GUNICORN_PID=./src/gunicorn.pid
 
-gunicorn --reload -c $GUNICORN_CONF --pid $GUNICORN_PID src.app:app
+# Create a tmux session called "Bespoke"
+tmux new-session -d -s Bespoke
+
+# Run the gunicorn server in the "Bespoke" session
+# gunicorn --reload -c $GUNICORN_CONF --pid $GUNICORN_PID src.app:app
+tmux send-keys -t Bespoke "gunicorn --reload -c $GUNICORN_CONF --pid $GUNICORN_PID src.app:app" C-m
