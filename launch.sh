52d4ffb58607fee71070e79a5bdd0859363ff1d0
Lodomo <lodomo.dev@gmail.com>
2026-01-06 11:31:37 -0800

    Fixing launch

--- launch.sh
+++ launch.sh
@@ -9,3 +9,6 @@ tmux new-session -d -s Bespoke
 # Run the gunicorn server in the "Bespoke" session
 # gunicorn --reload -c $GUNICORN_CONF --pid $GUNICORN_PID src.app:app
 tmux send-keys -t Bespoke "gunicorn --reload -c $GUNICORN_CONF --pid $GUNICORN_PID src.app:app" C-m
+
+# Echo to user how to attach to the tmux session
+echo "To attach to the 'Bespoke' tmux session, run: tmux attach -t Bespoke"
